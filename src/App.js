7e6da07bd2078baa04e093beece9146bf803fef6
dtorres1111 <45832414+dtorres1111@users.noreply.github.com>
2020-09-15 13:43:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
+
+const App = () => {
+    return (
+        <Router>
+            <Home path="/" />
+        </Router>
+    );
+};
+
+render(<App />, document.getElementById("root"));
